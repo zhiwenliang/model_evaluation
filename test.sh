f5378d8d1462c98fb31a9910c3bea6bc9b2e330d
zhiwen <zhiwen_liang@outlook.com>
2025-06-16 15:32:36 +0800

    1. update docs;2. add time out for xunfei api

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@
 # export MODEL_CONFIGS='[{"MODEL_CONFIG_ID":"qwen-turbo-latest","MODEL_TYPE":"API","PROMPT":"","TEMPERATURE":"","TOP_K":"","PRESENCE_PENALTY":"","API_TYPE":"OpenAI","API_URL":"https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions","API_KEY":"sk-8c019c61a5524a4fa6222ff0e9de9130","API_MODEL":"qwen-turbo-latest"}]'
 
 # spark
-export MODEL_CONFIGS='[{"MODEL_CONFIG_ID":"","MODEL_TYPE":"API","PROMPT":"","TEMPERATURE":"","TOP_P":"","PRESENCE_PENALTY":"","API_TYPE":"Spark","API_URL":"ws://172.30.212.36:80/default/public-cluster-npu/inference/bt-none-ec9bb83/inference//turing/v3/gpt","API_KEY":"","API_MODEL":""}]'
+export MODEL_CONFIGS='[{"MODEL_CONFIG_ID":"","MODEL_TYPE":"API","PROMPT":"","TEMPERATURE":"","TOP_P":"","PRESENCE_PENALTY":"","API_TYPE":"Spark","API_URL":"ws://172.30.212.36:80/default/public-cluster-npu/inference/bt-none-b9dc263/inference//turing/v3/gpt","API_KEY":"","API_MODEL":""}]'
 export DATASET_CONFIGS='[{"DATASET_CONFIG_ID":"gsm8k_1","DATASET_TYPE":"CUSTOM","EVALUATION_METRICS":"BLEU-4,rouge1,rouge2,rougeL,rougeLsum","BUILT_IN_DATASET":"","CUSTOM_DATASET_PATH":"/Users/humuh/source/my/model_evaluation/pre_dataset/gsm8k_0.jsonl"}]'
 export OPERATION_TYPE="INFERENCE"
 export INFERENCE_RESULT='/tmp/inference'
